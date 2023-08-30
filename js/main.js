1e595454de4d9d74644da810a7898e25881abdce
Vadym_Boiko <vadym.boiko1990@gmail.com>
2023-08-30 21:46:44 +0300

    base operators

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let a = 6;
+let b = 3;
+
+let sum = a + b;
+let sub = a - b;
+let mul = a * b;
+let div = a / b;
+let exp = a ** b;
+
+console.log('6 + 3 =', sum, '--//----(сложение)');
+console.log('6 - 3 =', sub, '--//----(вычитание)');
+console.log('6 * 3 =', mul, '--//---(умножение)');
+console.log('6 / 3 =', div, '--//----(деление)');
+console.log('6 ** 3 =', exp, '--//-(возведение в степень)');
